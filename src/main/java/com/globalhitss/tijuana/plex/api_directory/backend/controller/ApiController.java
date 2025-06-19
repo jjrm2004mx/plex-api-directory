502c1993ced39ee89faad035c68b85076773da4e
JOSE DE JESUS ROSALES MUNOZ <rosalesjj@globalhitss.com>
2025-06-19 12:14:54 -0700

    feat: integración de Prometheus

--- src/main/java/com/globalhitss/tijuana/plex/api_directory/backend/controller/ApiController.java
+++ src/main/java/com/globalhitss/tijuana/plex/api_directory/backend/controller/ApiController.java
@@ -10,17 +10,32 @@ import org.springframework.web.bind.annotation.RestController;
 import com.globalhitss.tijuana.plex.api_directory.backend.model.Api;
 import com.globalhitss.tijuana.plex.api_directory.backend.service.ApiService;
 
+// Contador de consumo de API 
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.web.bind.annotation.*;
+
 @RestController
 @RequestMapping("/apis")
 public class ApiController {
     private final ApiService apiService;
+    private final Counter apiControllerCounter;
 
-    public ApiController(ApiService apiService) {
+
+
+   /* public ApiController(ApiService apiService) {
+        this.apiService = apiService;
+    }*/
+
+     public ApiController(MeterRegistry registry, ApiService apiService) {
+        this.apiControllerCounter = registry.counter("api_controller_calls_total");
         this.apiService = apiService;
     }
 
     @GetMapping
     public List<Api> getAllApis() {
+        apiControllerCounter.increment();
         return apiService.getAllApis();
     }
 
